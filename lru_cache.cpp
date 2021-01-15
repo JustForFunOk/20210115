fa703c7bcf54cb91bb03ba702e173b5fcdc757e9
SunJian <slgsunjian@163.com>
2021-01-15 23:24:50 +0800

    reduce time complexity from On to O1

--- lru_cache.cpp
+++ lru_cache.cpp
@@ -19,22 +19,17 @@ public:
      */
     int get(int key) {
         // find element in map
-        auto find_result = key_val_map_.find(key);
+        auto find_result = key_list_iterator_map_.find(key);
 
-        if(find_result != key_val_map_.end())
+        if(find_result != key_list_iterator_map_.end())
         {
             // find it, adjust element position in list
-            for(auto it = key_list_.begin(); it != key_list_.end(); ++it)
-            {
-                if(*it == key)
-                {
-                    // change the key position to the end of list
-                    key_list_.erase(it);
-                    key_list_.push_back(key);
-                }
-            }
-
-            return find_result->second;
+            key_value_list_.push_back(std::make_pair(key, find_result->second->second));
+            key_value_list_.erase(find_result->second);
+            auto last_ele_iterator = key_value_list_.end();
+            find_result->second = --last_ele_iterator; // update the iterator in the map
+            
+            return find_result->second->second;
         }
 
         return NOT_FOUND;
@@ -46,46 +41,51 @@ public:
      * @return: nothing
      */
     void set(int key, int value) {
-        auto find_result = key_val_map_.find(key);
+        auto find_result = key_list_iterator_map_.find(key);
 
         // exist in map, update the value, change position
-        if(find_result != key_val_map_.end())
+        if(find_result != key_list_iterator_map_.end())
         {
-            find_result->second = value;  // update value
+            // update value
+            find_result->second->second = value;  
 
-            for(auto it = key_list_.begin(); it != key_list_.end(); ++it)
-            {
-                if(*it == key)
-                {
-                    key_list_.erase(it);
-                    key_list_.push_back(key);
-                }
-            }
+            // change position to the end of list
+            key_value_list_.push_back(std::make_pair(key, value));
+            key_value_list_.erase(find_result->second);
+
+            // update the iterator in the map
+            auto last_ele_iterator = key_value_list_.end();
+            find_result->second = --last_ele_iterator; 
         }
-        else  // donot exist
+        else  // not exist
         {
-            // full
-            if(key_val_map_.size() == capacity_)
+            // full, erase the oldest element
+            if(key_value_list_.size() == capacity_)
             {
-                // find oldest_key in map and erase
-                int oldest_key = key_list_.front();
-                auto find_result = key_val_map_.find(oldest_key);
-                key_val_map_.erase(find_result);
+                // get the key of oldest value
+                int oldest_key = key_value_list_.front().first;
+
+                // erase the oldest ele in map
+                auto find_result = key_list_iterator_map_.find(oldest_key);
+                key_list_iterator_map_.erase(find_result);
 
                 // erase the oldest ele in list
-                key_list_.pop_front();
+                key_value_list_.pop_front();
             }
             
-            // insert ele to map and the end of list
-            key_val_map_[key] = value;
-            key_list_.push_back(key);
+            // insert key-value pair to the end of list
+            key_value_list_.push_back(std::make_pair(key, value));
+            
+            // insert key and iterator to the map
+            auto last_ele_iterator = key_value_list_.end();
+            key_list_iterator_map_[key] = --last_ele_iterator; // update the iterator to the end of list
         }       
     }
 
 private:
     int capacity_;
-    std::list<int> key_list_;  // store the order, list: oldest(front) <------> newest(end)
-    std::unordered_map<int, int> key_val_map_;  // store key and value
+    std::list<std::pair<int, int>> key_value_list_;  // store the key-value pair, list order: oldest(front) <------> newest(end)
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> key_list_iterator_map_;  // store key and the position of value in list 
 };
 
 int main() {
