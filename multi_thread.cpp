3ee7588777a3ee319c8e0129a734d819a3990c6a
SunJian <slgsunjian@163.com>
2021-01-16 12:22:55 +0800

    add producer consumer multi-thread demo

--- multi_thread.cpp
+++ multi_thread.cpp
@@ -15,3 +15,135 @@
 //     double data[5];
 // }
 // c++ 14
+
+// g++ multi_thread.cpp -o multi_thread -std=c++14 -pthread
+#include <queue>
+#include <thread>
+#include <random>
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+
+struct A_struct{
+    int a;
+} product_A;
+
+struct B_struct{
+    double data[5];
+};
+std::queue< B_struct > product_B_queue;
+
+double sum_b_all = 0;
+
+// mutex lock
+std::mutex product_B_queue_mtx;
+std::mutex mtx;
+
+// condition_variable
+std::condition_variable queue_has_product;
+
+// generate ramdom number
+std::random_device rd; // obtain a random number from hardware
+std::mt19937 gen(rd()); // seed the generator
+std::uniform_int_distribution<int> distr_duration(100, 1000); // duration range: 100-1000 ms
+std::uniform_int_distribution<int> distr_A(1, 100); // range: 1-100
+std::uniform_int_distribution<int> distr_B(0, 100); // range: 0-100
+
+/*
+ * produce a random number(1-100) in random duration(1-100ms)
+ */
+void producer_A()
+{
+    while(1)
+    {   
+        // sleep for random milliseconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(distr_duration(gen)));  
+
+        // update random number
+        product_A.a = distr_A(gen);
+        // std::cout << product_A.a << std::endl;
+    }
+}
+
+/*
+ * produce 5 random numbers(0-1) in random duration(100-1000ms)
+ */
+void producer_B()
+{
+    while(1)
+    {   
+        // sleep for random milliseconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(distr_duration(gen)));  
+        
+        // produce 5 random number
+        B_struct product_B;
+        for(int i = 0; i < 5; ++i)
+        {
+            product_B.data[i] = distr_B(gen) / 100.0; // range: 0-1, precision 0.01
+        }
+
+        // push random number to queue
+        std::unique_lock<std::mutex> queue_lock(product_B_queue_mtx);
+        product_B_queue.push(product_B);
+        queue_lock.unlock();
+
+        queue_has_product.notify_all();  // notify consumer
+    }
+}
+
+void consumer_C()
+{
+    while(1)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        queue_has_product.wait(lock);  // wait for new product
+      
+        std::unique_lock<std::mutex> queue_lock(product_B_queue_mtx);
+        // get product_B
+        auto product_B = product_B_queue.front();
+        // pop product_B 
+        product_B_queue.pop();
+        queue_lock.unlock();
+        
+        // calculate sum_b
+        double sumb = 0;
+        for(int i = 0; i < 5; ++i)
+        {
+            sumb += product_B.data[i];  // cal sum
+        }
+        
+        if(!(product_A.a % 2))
+        {
+            sumb *= -1;  // even
+        }
+        sum_b_all += sumb;       
+    }
+}
+
+void consumer_D()
+{
+    while(1)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));  // sleep for 1 second
+
+        std::cout << sum_b_all << std::endl;
+    }
+}
+
+int main()
+{
+    // spawn thread
+    std::thread thread_A(producer_A);
+    std::thread thread_B(producer_B);
+    std::thread thread_C(consumer_C);
+    std::thread thread_D(consumer_D);
+
+    // wait for thread finish
+    thread_A.join();
+    thread_B.join();
+    thread_C.join();
+    thread_D.join();
+
+    return 0;
+}
